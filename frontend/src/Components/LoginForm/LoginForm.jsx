02d1096c16e2b7b532dfa08b5858c3df43f3c6a6
Maxim Degtiarev <mxim.degt@gmail.com>
2024-11-06 19:39:32 -0300

    Login page finished

--- frontend/src/Components/LoginForm/LoginForm.jsx
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./LoginForm.css";
 const LoginForm = () => {
     return (
-        <div>
+        <div className="login-container">
             <h2> Login</h2>
-            <h3>Welcome Back</h3>
-            <div>
-                <label htmlFor="email">Login</label>
-                <input type="email" name="email" id="email" />
+            {/* <h3>Welcome back!</h3> */}
+            <form action="" className="login-form">
+                <div className="form-group">
+                    <label htmlFor="email">Login</label>
+                    <input type="email" name="email" id="email" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div className="remember-container">
+                    <div>
+                        <input type="checkbox" name="remember" id="remember" />
+                        <label htmlFor="remember">Remember me</label>
+                    </div>
+                    <Link to="/forgot-password" className="link link-forgot">
+                        Forgot Password?
+                    </Link>
+                </div>
+                <div className="btn-container">
+                    <button className="btn btn-signin">Sign In</button>
+                </div>
+            </form>
+            <div className="link-container">
+                <span>
+                    New to Bancax?{" "}
+                    <Link className="link link-signup" to={"/register"}>
+                        Sign up!
+                    </Link>
+                </span>
             </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" />
-            </div>
-            <div>
-                <input type="checkbox" name="remember" id="remember" />
-                <label htmlFor="remember">Remember me</label>
-                <Link to="/forgot-password">Forgot Password?</Link>
-            </div>
-            <div>
-                <button type="submit" className="btn btn-login">
-                    Sign In
-                </button>
+            <div className="copyright-container">
+                <span>
+                    © Maxim Degtiarev 2024. Only for portfolio purposes.
+                </span>
             </div>
         </div>
     );
